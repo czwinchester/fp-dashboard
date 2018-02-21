7a2f4983a3e956c699d5a6a92ea72889b89f9a25
Euan Cairncross <euan@cairncross.io>
2018-02-21 16:35:52 +0000

    Fix build status sorting on load

--- src/components/BuildsTable.vue
+++ src/components/BuildsTable.vue
@@ -5,7 +5,7 @@
       <div class="builds-table-header-row columns">
         <span class="builds-table-header column is-2" v-for="heading in headings">{{heading}}</span>
       </div>
-      <BuildsTableRow :headings="headings" :build="build" v-for="build in builds"></BuildsTableRow>
+      <BuildsTableRow :headings="headings" :build="build" v-for="build in sortedBuilds"></BuildsTableRow>
     </div>
   </div>
 </template>
@@ -29,20 +29,22 @@ export default {
     BuildsTableHeader,
     BuildsTableRow
   },
-  mounted() {
-    this.builds.sort((a, b) => {
-      if ((a.status === 'No build' && b.status === 'On time') ||
-        (a.status === 'Delayed' && b.status === 'On time') ||
-        (a.status === 'No build' && b.status === 'Delayed')) {
-        return -1
-      }
-      else if ((a.status === 'On time' && b.status === 'No build') ||
-        (a.status === 'On time' && b.status === 'Delayed') ||
-        (a.status === 'Delayed' && b.status === 'No build')) {
-        return 1
-      }
-      return 0
-    })
+  computed: {
+    sortedBuilds: function () {
+      return this.builds.sort((a, b) => {
+        if ((a.status === 'No build' && b.status === 'On time') ||
+          (a.status === 'Delayed' && b.status === 'On time') ||
+          (a.status === 'No build' && b.status === 'Delayed')) {
+          return -1
+        }
+        else if ((a.status === 'On time' && b.status === 'No build') ||
+          (a.status === 'On time' && b.status === 'Delayed') ||
+          (a.status === 'Delayed' && b.status === 'No build')) {
+          return 1
+        }
+        return 0
+      })
+    }
   }
 }
 </script>
